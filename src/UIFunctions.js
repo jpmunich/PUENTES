ae383a1d67665d77b0b743996d112b037007f252
jpmunich <parkercoyle8@gmail.com>
2023-12-26 15:36:43 -0500

    Element creation functions

---
+++ src/UIFunctions.js
@@ -0,0 +1,34 @@
+const createElement = (type, parent) => {
+  const element = document.createElement(type);
+  parent.appendChild(element);
+  return element;
+};
+
+const createTextElement = (type, parent, text) => {
+  const element = document.createElement(type);
+  parent.appendChild(element);
+  element.innerText = text;
+  return element;
+};
+
+const createElementWithClass = (type, parent, className) => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  parent.appendChild(element);
+  return element;
+};
+
+const createImage = (source, parent, className) => {
+  const element = document.createElement("img");
+  element.classList.add(className);
+  element.src = source;
+  parent.appendChild(element);
+  return element;
+};
+
+export {
+  createElement,
+  createTextElement,
+  createElementWithClass,
+  createImage,
+};
